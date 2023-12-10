d1669c2f81d7b158392ab9726a328ce11fe3b879
danielmyska <st67072@upce.cz>
2023-12-10 17:13:08 +0100

    Lord have mercy

--- View/UserProfile.php
+++ View/UserProfile.php
@@ -44,12 +44,15 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
+                <li class="nav-item ">
                     <a class="nav-link" href="Messages.php">InBox</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="../index.php">Odhlásit</a>
                 </li>
                 <?php
                     if($user['permission'] == 1) {
-                        echo '<li class="nav-item"> <a class="nav-link" href="Users.php">Uzivatele</a> </li>';
+                        echo '<li class="nav-item"> <a class="nav-link float-left" href="Users.php">Uzivatele</a> </li>';
                     }
                 ?>
             </ul>
@@ -98,6 +101,8 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
 
     </form>
 </section>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 </html>
